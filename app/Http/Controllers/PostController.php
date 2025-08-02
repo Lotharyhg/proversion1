3e18b430c399c99aa9a65b679cbd3a976028ad7c
Lotharyhg <lothar.hernandez@utgz.edu.mx>
2025-08-02 13:35:53 -0600

    Agregar posts y mostrarlas

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // return view('posts/index');
+
+        return view ('posts/index', [
+            'posts' => Post::with('user')->latest()->get(),
+        ]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $dataValidates = $request->validate([
+            'message' => ['required', 'min:8', 'max:255'],
+        ]);
+
+        // Post::create([
+        //     'message' => $dataValidates,
+        //     'user_id' => auth()->id(),
+        // ]);
+
+        $request->user()->posts()->create($dataValidates); // crear registro a traves de la relación de user
+
+        return to_route('posts.index')->with ('status', __('Post Created Sucessfully!'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,14 +8,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Creación de rutas para el foro
+// Ruta para llamar la funcióin de index y mostrar publicaciones o posteos
+Route::get('/post', [App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
+// Ruta para crear el registro en BD de Posts
+Route::post('/post', [App\Http\Controllers\PostController::class, 'store'])->name('posts.store');
+// Ruta pra el formulario de edición de posts
+Route::get('/post/{post}/edit', [App\Http\Controllers\PostController::class, 'edit'])->name('posts.edit');
+// Ruta para actualizar los post en la base de datos
+Route::patch('/post/{post}', [App\Http\Controllers\PostController::class, 'update'])->name('posts.update');
+// Ruta para eliminar los posts o publicaciones
+Route::delete('/post/{post}', [App\Http\Controllers\PostController::class, 'destroy'])->name('posts.destroy');
 
 require __DIR__.'/auth.php';
